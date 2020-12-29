02a18f53a12699083fe58a247f1e6a3311ef5d8b
GreenPepper <k.reinders@gmx.net>
2020-12-29 03:49:48 +0100

    Projektdateien hinzufügen.

---
+++ Blutalkoholgehalt/Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace Blutalkoholgehalt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.alkAmountTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.amountTextBox = new System.Windows.Forms.TextBox();
+            this.submitBtn = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.genderList = new System.Windows.Forms.ListBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.kgKGTextBox = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.alkAmountTextBox);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.amountTextBox);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(189, 106);
+            this.panel1.TabIndex = 0;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(98, 76);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(15, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "%";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(98, 24);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "ml";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(127, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Alkoholgehalt (in Prozent)";
+            // 
+            // alkAmountTextBox
+            // 
+            this.alkAmountTextBox.Location = new System.Drawing.Point(3, 73);
+            this.alkAmountTextBox.Name = "alkAmountTextBox";
+            this.alkAmountTextBox.Size = new System.Drawing.Size(89, 20);
+            this.alkAmountTextBox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Menge (in ml)";
+            // 
+            // amountTextBox
+            // 
+            this.amountTextBox.Location = new System.Drawing.Point(3, 21);
+            this.amountTextBox.Name = "amountTextBox";
+            this.amountTextBox.Size = new System.Drawing.Size(89, 20);
+            this.amountTextBox.TabIndex = 0;
+            // 
+            // submitBtn
+            // 
+            this.submitBtn.Location = new System.Drawing.Point(13, 339);
+            this.submitBtn.Name = "submitBtn";
+            this.submitBtn.Size = new System.Drawing.Size(389, 58);
+            this.submitBtn.TabIndex = 1;
+            this.submitBtn.Text = "Suchen";
+            this.submitBtn.UseVisualStyleBackColor = true;
+            this.submitBtn.Click += new System.EventHandler(this.submitBtn_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.genderList);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.label8);
+            this.panel2.Controls.Add(this.label7);
+            this.panel2.Controls.Add(this.kgKGTextBox);
+            this.panel2.Location = new System.Drawing.Point(213, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(189, 106);
+            this.panel2.TabIndex = 1;
+            // 
+            // genderList
+            // 
+            this.genderList.FormattingEnabled = true;
+            this.genderList.Items.AddRange(new object[] {
+            "Männlich",
+            "Weiblich"});
+            this.genderList.Location = new System.Drawing.Point(3, 21);
+            this.genderList.Name = "genderList";
+            this.genderList.Size = new System.Drawing.Size(120, 30);
+            this.genderList.TabIndex = 2;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(98, 76);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(19, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "kg";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(3, 5);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(123, 13);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Genetisches Geschelcht";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(3, 57);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(34, 13);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "kgKG";
+            // 
+            // kgKGTextBox
+            // 
+            this.kgKGTextBox.Location = new System.Drawing.Point(3, 73);
+            this.kgKGTextBox.Name = "kgKGTextBox";
+            this.kgKGTextBox.Size = new System.Drawing.Size(89, 20);
+            this.kgKGTextBox.TabIndex = 8;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(280, 121);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "label6";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(78, 121);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(35, 13);
+            this.label9.TabIndex = 4;
+            this.label9.Text = "label9";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(183, 227);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(41, 13);
+            this.label10.TabIndex = 5;
+            this.label10.Text = "label10";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(414, 409);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.submitBtn);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Blutalkoholgehalt";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox alkAmountTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox amountTextBox;
+        private System.Windows.Forms.Button submitBtn;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox kgKGTextBox;
+        private System.Windows.Forms.ListBox genderList;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ Blutalkoholgehalt/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Blutalkoholgehalt
+{
+    public partial class Form1 : Form
+    {
+        private float genderConst;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void Calculate(int amount, float alkAmount, int gender, int weight)
+        {
+            // Step 1:
+            float first = amount * (alkAmount/100);
+
+            // Step 2:
+            if (gender == 0) genderConst = 0.6f; // Männlich
+            if (gender == 1) genderConst = 0.7f; // Weiblich
+
+            float seccond = genderConst * weight;
+
+            label9.Text = first.ToString();
+            label6.Text = seccond.ToString();
+
+            label10.Text = (first / seccond).ToString();
+
+            return;
+        }
+        private void submitBtn_Click(object sender, EventArgs e)
+        {
+            Calculate(Int16.Parse(amountTextBox.Text), Int16.Parse(alkAmountTextBox.Text), genderList.SelectedIndex, Int16.Parse(kgKGTextBox.Text));
+        }
+    }
+}
